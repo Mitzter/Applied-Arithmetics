8caabd60ec8e18a21f7c0db45c7e111001cbd57f
Loran Abbas <loranabbas@abv.bg>
2022-06-02 19:26:40 +0300

    Add project files.

---
+++ Applied Arithmetics/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Func<List<int>, List<int>> add = list => list.Select(number => number += 1).ToList();
+
+            Func<List<int>, List<int>> multiply = list => list.Select(number => number *= 2).ToList();
+
+            Func<List<int>, List<int>> subtract = list => list.Select(number => number -= 1).ToList();
+
+            Action<List<int>> print = list => Console.WriteLine(String.Join(" ", list));
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+
+                        numbers = add(numbers);
+                        break;
+                    case "multiply":
+
+                        numbers = multiply(numbers);
+                        break;
+                    case "subtract":
+                        numbers = subtract(numbers);
+
+                        break;
+                    case "print":
+                        print(numbers);
+
+
+                        break;
+                }
+
+
+
+
+
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
